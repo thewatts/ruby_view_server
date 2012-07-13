6ef83fd0357f37b1c9fa448055f8d238bfb07fba
thewatts <reg@nathanielwatts.com>
2012-07-13 10:59:50 -0500

    Complete Using ERB

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,15 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+array = %w[cat car bat ball blam]
+template = ERB.new "These are the values of the array <%= array %> "
+puts template.result(binding)
+
+array << "WEEMA"
+template = ERB.new "I just added WEEMA to the array... SEEEEE?!?!?! :: <%= array %>"
+puts template.result(binding)
+
+hash = {first: "Thomas", last: "Little", occupation: "Captain", age: 26, bearded: true}
+template = ERB.new "Check out this hash of Thomas: <%= hash %>"
+puts template.result(binding)
